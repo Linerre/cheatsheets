b4784c0fb67c096a6ddf928d0720c5354fb1b423
Linerre <leon.errelin@outlook.com>
2023-08-28 18:08:54 +1000

    Finish Q2

--- comp6240/assignments/assign1/myqueries.sql
+++ comp6240/assignments/assign1/myqueries.sql
@@ -2,10 +2,25 @@
 -- Uid:  u7753813
 
 -- Q1
--- TODO: count the result instead of returning it
 SELECT COUNT(last_name) FROM person
   WHERE
   year_born > 1974 AND
   last_name LIKE '%e';
 
 -- Q2
+SELECT ROUND(AVG(run_time), 2) FROM
+  (
+    SELECT DISTINCT
+    restriction.title,
+    restriction.production_year,
+    description,
+    restriction.country,
+    run_time
+    FROM restriction, movie
+    WHERE
+    restriction.country = 'USA' AND
+    restriction.description = 'R' AND
+    restriction.production_year > 1991
+  ) AS mv;
+
+-- Q3
