dd4b012d2867080625a07e9b3f40f79ecab02b11
Linerre <leon.errelin@outlook.com>
2023-08-29 13:38:11 +1000

    Finish assign
    ** Q3,4,5,6,7,8,9,10 done

--- comp6240/assignments/assign1/myqueries.sql
+++ comp6240/assignments/assign1/myqueries.sql
@@ -1,5 +1,5 @@
--- Name: Zhiren Lin
--- Uid:  u7753813
+-- name: Zhiren Lin
+-- uid:  u7753813
 
 -- Q1
 SELECT COUNT(last_name) FROM person
@@ -24,3 +24,113 @@ SELECT ROUND(AVG(run_time), 2) FROM
   ) AS mv;
 
 -- Q3
+SELECT COUNT(crew_number) FROM
+  (
+    SELECT COUNT(id) AS crew_number, title FROM crew
+    GROUP BY title
+    HAVING COUNT(id) = 2
+  ) AS cn;
+
+-- Q4
+SELECT id, first_name || ' ' || last_name AS full_name FROM -- director (id, name)
+  (
+    SELECT * FROM
+    (
+      SELECT * FROM director AS d -- director id without an award
+      LEFT JOIN director_award AS da
+      USING (title)
+      WHERE RESULT IS NULL
+    ) nad
+    RIGHT JOIN person AS p        -- director (id, name, movie, year ...) without an award
+    USING (id)
+    WHERE NOT title IS NULL
+  ) dnoa;
+
+-- Q5
+SELECT COUNT(id) AS num_of_movies, first_name, last_name FROM
+  (
+    SELECT * FROM
+    (
+      SELECT * FROM director          -- director(s) who have directed
+      LEFT JOIN movie
+      USING (title) WHERE major_genre = 'action'
+    ) am
+    LEFT JOIN person AS p
+    USING (id)
+  ) fi GROUP BY COUNT(id), first_name ORDER BY first_name;
+
+-- Q6
+SELECT
+  ROUND(
+    100.0 * COUNT(ca.title)/(select count(title) from movie where major_genre = 'comedy'),
+    2
+  ) AS "Aussie Comedy Prop. (%)"
+  FROM
+  (SELECT title FROM movie WHERE major_genre = 'comedy' AND country = 'Australia') ca;
+
+-- Q7
+WITH final AS (
+  SELECT COUNT(title) AS total_won, title, production_year FROM
+  (
+    SELECT
+    a.title, a.production_year, a.award_name AS dir_award, a.year_of_award AS dir_award_year,
+    b.award_name AS act_award, b.year_of_award AS act_award_year, b.category
+    FROM director_award a
+    LEFT JOIN actor_award b
+    USING(title, production_year)
+    WHERE NOT description IS NULL
+    ORDER BY title
+  ) awards
+  GROUP BY title, production_year
+) SELECT title, production_year FROM final
+  WHERE total_won = (SELECT MIN(total_won) FROM final);
+
+-- Q8
+SELECT COUNT(award_num) FROM
+  (
+    SELECT COUNT(title) AS award_num, title, production_year FROM
+    (
+      SELECT
+      *
+      FROM movie_award AS A
+      FULL JOIN director_award
+      USING (title, production_year, year_of_award)
+      FULL JOIN writer_award
+      USING (title, production_year, year_of_award)
+      FULL JOIN actor_award
+      USING (title, production_year, year_of_award)
+      FULL JOIN crew_award
+      USING (title, production_year, year_of_award)
+      ORDER BY title
+    ) awards
+    GROUP BY title, production_year
+    ORDER BY award_num;
+  ) award_mv;
+
+-- Q9
+SELECT DISTINCT CONCAT('(', title, ', ', production_year, ')') AS pair FROM
+  (
+    SELECT
+    *
+    FROM movie_award AS A
+    FULL JOIN director_award
+    USING (title, production_year, year_of_award)
+    FULL JOIN writer_award
+    USING (title, production_year, year_of_award)
+    FULL JOIN actor_award
+    USING (title, production_year, year_of_award)
+    FULL JOIN crew_award
+    USING (title, production_year, year_of_award)
+    ORDER BY title
+  ) awards
+  ORDER BY pair;
+
+-- Q10
+SELECT id, first_name, last_name
+  FROM person
+  RIGHT JOIN
+  (
+    SELECT id FROM writer WHERE credits <> ''
+  ) AS coop
+  USING(id)
+  ORDER BY first_name;
